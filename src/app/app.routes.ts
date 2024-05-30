63b44e8dcab36e1ee1d703ad3fdccfc0afa459f9
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-05-30 15:45:50 +0200

    adding basic components & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+];
