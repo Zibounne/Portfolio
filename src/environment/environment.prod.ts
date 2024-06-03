bad99a377609b40c0f3166a95698ceabe295abc0
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-06-03 19:33:54 +0200

    Adding : workflow secret

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    emailjsServiceID: 'EMAILJS_SERVICE_ID',
+    emailjsTemplateID: 'EMAILJS_TEMPLATE_ID',
+    emailjsUserID: 'EMAILJS_USER_ID'
+  };
