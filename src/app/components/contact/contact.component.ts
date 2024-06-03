ced1e515a9fda570dcb8ac016c6fbb5d00d747b5
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-06-03 18:45:04 +0200

    Adding : About & Contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { environment } from '../../../environment/environment';
 
 @Component({
   selector: 'app-contact',
@@ -12,6 +14,26 @@ export class ContactComponent {
 
   constructor(private titleService:Title) {
     this.titleService.setTitle("Portfolio | Contact");
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    
+    emailjs.sendForm(
+      environment.emailjsServiceID, 
+      environment.emailjsTemplateID, 
+      e.target as HTMLFormElement, 
+      environment.emailjsUserID)
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        const confirmationMessage = document.getElementById('confirmationMessage');
+        if (confirmationMessage) {
+          confirmationMessage.classList.remove('hidden');
+          confirmationMessage.classList.add('block');
+        }
+      }, (error) => {
+        console.log(error.text);
+      }); 
   }
   
 }
