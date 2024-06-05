6949d65c8f2bb18ab18a30bb11f4ed2161951dd3
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-06-05 09:37:25 +0200

    Finish : Projects page

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
 
@@ -8,10 +9,42 @@ import { Title } from '@angular/platform-browser';
   imports: [],
   templateUrl: './projects.component.html',
 })
+
 export class ProjectsComponent {
 
-  constructor(private titleService:Title) {
+  private lastScrollTop: number = 0;
+  private projectsArrow: HTMLElement | null = null;
+
+  constructor
+  (
+    private titleService: Title,
+    @Inject(DOCUMENT) private document: Document
+  ) 
+  {
+
+  }
+
+  ngOnInit(): void {
     this.titleService.setTitle("Portfolio | Projects");
+    this.projectsArrow = this.document.getElementById("projectsArrow");
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const scrollTop = this.document.documentElement.scrollTop;
+
+    if (this.projectsArrow) {
+      if (scrollTop > this.lastScrollTop) {
+        this.projectsArrow.style.visibility = 'hidden';
+        this.projectsArrow.style.opacity = '0';
+        this.projectsArrow.style.transition = 'visibility 0s, opacity 0.5s linear';
+      } else {
+        this.projectsArrow.style.visibility = 'visible';
+        this.projectsArrow.style.opacity = '1';
+      }
+    }
+
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
   }
 
 }
