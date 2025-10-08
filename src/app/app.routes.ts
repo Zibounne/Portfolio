2525042a07b7fe59b06df33386f3d2048421daa2
Zibounne <tarik.dahhane.pro@gmail.com>
2025-10-08 13:25:45 +0200

    Update Projects & Delete About me page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'projects', component: ProjectsComponent },
-    { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: '**', component: NotFoundComponent }
 ];
